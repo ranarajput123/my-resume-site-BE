008f682ae6f591d43e1c4d7d3eeae80036cf18ce
Sarmad Zargham Nasir <ranasarmad1996@gmail.com>
2021-05-24 03:14:19 +0100

    mongo db atlas configured

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 const {
-	MONGO_URL,
-	MONGO_P,
-	MONGO_U
+	DEFAULT_DATABASE_NAME,
+	DATABASE_USER,
+	DATABASE_PASS,
+	NODE_ENV
+	// MONGO_URL,
+	// MONGO_P,
+	// MONGO_U
 	// eslint-disable-next-line no-undef
 } = process.env;
 
-const url = MONGO_URL ? `mongodb://${MONGO_U}:${MONGO_P}@${MONGO_URL}:27017/react?authSource=admin` : 'mongodb://localhost:27017/react';
+// const url = MONGO_URL ? `mongodb://${MONGO_U}:${MONGO_P}@${MONGO_URL}:27017/react?authSource=admin` : 'mongodb://localhost:27017/react'; // FOR AWS EKS 
+const url = NODE_ENV === 'development' ?
+	'mongodb://localhost:27017/react' :
+	`mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@my-site-db-cluster0.rxni0.mongodb.net/${DEFAULT_DATABASE_NAME}?retryWrites=true&w=majority`;
 (async () => {
 	try {
 		await mongoose.connect(
